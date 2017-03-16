e7b8f532bc1455c72648619180696702c1986cc6
Kate Datsenko <kate.datsenko@gmail.com>
2017-03-16 15:02:02 +0200

    Пользовательские кнопки TinyMCE

--- functions.php
+++ functions.php
@@ -35,3 +35,47 @@ function addAdminMenu(){
 function renderThemeMenu(){
     _e('Sub theme by Kate', MY_THEME_TEXTDOMAIN);
 }
+
+
+add_shortcode( 'twentysixteen_child_guest_book', 'guestBookShortcode');
+function guestBookShortcode(){
+    $output = '';
+    $output .= '<form  method="post">
+                    <label>'.__('User name', MY_THEME_TEXTDOMAIN ).'</label>
+                    <input type="text" name="twentysixteen_child_" class="twentysixteen-child-name">
+                    <label>'.__('Message', MY_THEME_TEXTDOMAIN ).'</label>
+                    <textarea name="twentysixteen_child_message" class="twentysixteen-child-message"></textarea>
+                    <button class="twentysixteen-child-btn-add" >'.__('Add', MY_THEME_TEXTDOMAIN ).'</button>                   
+                </form>';
+    return $output;
+}
+add_action('media_buttons','addMediaButtons');
+function addMediaButtons(){
+    $button = '<a href="#" id="guestBookShortcodeButton" class="su-generator-button button">'
+        .__('Insert shortcode', MY_THEME_TEXTDOMAIN).'</a>';
+    echo $button;
+}
+add_action('admin_enqueue_scripts', 'loadScriptAdmin');
+function loadScriptAdmin($hook){
+    wp_enqueue_script(
+        'twentysixteen_child_admin_main', //$handle
+        get_stylesheet_directory_uri() .'/assets/js/twentysixteen-child-admin-main.js', //$src
+        array(
+            'jquery',
+        )
+    );
+}
+add_action( 'init', 'setupTinyMCE' );
+function setupTinyMCE(){
+    add_filter( 'mce_external_plugins', 'addTinyMCE' );
+    add_filter( 'mce_buttons', 'addTinyMCEToolbar' );
+}
+function addTinyMCE( $plugin_array ) {
+    $plugin_array['twentysixteen_child_custom_class'] = get_stylesheet_directory_uri()
+        . '/assets/js/MYTinyMCE.js';
+    return $plugin_array;
+}
+function addTinyMCEToolbar( $buttons ) {
+    array_push( $buttons, 'guest_book_shortcode_button' );
+    return $buttons;
+}
