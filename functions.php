1e3bfc08d457dc587873068956d03f4e4ae2f785
Kate Datsenko <kate.datsenko@gmail.com>
2017-03-16 12:50:48 +0200

    File functions.php

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+
+add_filter('wp_title', 'filterTitle');
+function filterTitle($title) {
+    return $title.' - Twenty Sixteen Child';
+}
+add_filter('the_content', 'filterTheContent');
+function filterTheContent($content) {
+    return $content.' - Twenty Sixteen Child';
+}
